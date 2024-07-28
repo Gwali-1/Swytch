6b9e9d13f99c53945c92c431b18a14e95a048b22
MAIRO <gwalisam37@gmail.com>
2024-07-28 08:45:51 +0000

    testing logginf support in statuc file server example

--- Examples/StaticFileServer/Program.cs
+++ Examples/StaticFileServer/Program.cs
@@ -1,29 +1,42 @@
 ﻿using System.Net;
+using Microsoft.Extensions.Logging;
 using Swytch.App;
 using Swytch.Structures;
 using Swytch.utilities;
 
 var server = new SwytchApp();
 
+using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+ILogger logger = factory.CreateLogger<Program>();
+
 server.AddMiddleWare(async c =>
 {
-    Console.WriteLine($" {c.Request.ProtocolVersion} {c.Request.HttpMethod} {c.Request.Url}");
+    logger.LogInformation(" {c.Request.ProtocolVersion} {c.Request.HttpMethod} {c.Request.Url}",
+        c.Request.ProtocolVersion, c.Request.HttpMethod, c.Request.Url.AbsolutePath);
 });
 
 //action method can be writen like this
 Func<RequestContext, Task> evening = async c =>
 {
-    await Utilities.ServeFile(c,"goodevening.html",HttpStatusCode.OK);
-
+    await Utilities.ServeFile(c, "goodevening.html", HttpStatusCode.OK);
 };
 
 
+//file server
+
+Func<RequestContext, Task> fserver = async c =>
+{
+    string filename;
+    _ = c.PathParams.TryGetValue("name", out filename);
+    await Utilities.ServeFile(c, filename, HttpStatusCode.OK);
+};
+
+server.AddAction("GET", "/file/{name}", fserver);
+
 server.AddAction("GET", "/", evening);
 
-server.AddAction("GET","/travelling", async c =>
-{
-    await Utilities.ServeFile(c,"traveling.html",HttpStatusCode.OK);
-});
+server.AddAction("GET", "/travelling",
+    async c => { await Utilities.ServeFile(c, "traveling.html", HttpStatusCode.OK); });
 
 
 await server.Listen("http://localhost:8080/");
